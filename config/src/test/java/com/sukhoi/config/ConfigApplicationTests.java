515ad6fe5f854f51707590394b3dd7cf64e6a085
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2025-12-11 23:03:47 +0700

    chore(init): init config and user service

---
+++ config/src/test/java/com/sukhoi/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukhoi.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
